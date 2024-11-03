190ae8e9dd746e2efc823a309e05627d0c4a54ce
Atiqah <natiqah04.23@ichat.sp.edu.sg>
2024-11-03 20:53:28 +0800

    Commit all changes, added additional test functions, exercise 1

--- Lab3.py
+++ Lab3.py
@@ -12,6 +12,12 @@ def bubble_sort(arr, sorting_order):
     # Get number of elements in the list
     n = len(arr_result)
 
+    if n == 0:
+        return 0
+    
+    if not all(isinstance(x, int) for x in arr):
+        return 2
+
     if n < 10:
         # Traverse through all array elements
         for i in range(n - 1):
@@ -34,7 +40,7 @@ def bubble_sort(arr, sorting_order):
                     # Return an empty array
                     arr_result = []
     else:
-        arr_result = -1
+        arr_result = 1
 
     return arr_result
 
--- Test_Lab3.py
+++ Test_Lab3.py
@@ -28,3 +28,18 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_large_input():
+    input_arr = [64, 34, 25, 12, 22, 11, 90, 45, 56, 78]
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert result == 1
+
+def test_bubble_sort_empty():
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert result == 0
+
+def test_bubble_sort_non_integer():
+    input_arr = [64, 34, 'a', 12, 22, 11]
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+    assert result == 2
